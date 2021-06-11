5e824a08aa639e8596cbef42ea7751db63c57e52
Masa <katsukatsu.masa.0522@gmail.com>
2021-06-11 23:08:24 +0900

    first commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <hr>
+    <h2>Products</h2>
+    <template>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          {{ product.title }} - {{ product.price }}
+        </li>
+      </ul>
+    </template>
+  </div>
+</template>
+
+<script>
+import shop from '@/api/shop.js'
+export default {
+  name: 'ProductList',
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      products : [],
+    }
+  },
+  created(){
+    shop.getProducts( products => this.products = products);
+  }
+}
+</script>
