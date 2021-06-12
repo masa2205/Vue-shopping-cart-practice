b7bb452bab34356fddb65fd4e30c44ce7329d006
Masa <katsukatsu.masa.0522@gmail.com>
2021-06-12 09:26:48 +0900

    import shop data

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,23 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+import shop from '@/api/shop.js'
+
 export default new Vuex.Store({
   state: {
+    products: [],
   },
   mutations: {
+    setProducts(state, products) {
+      state.products = products;
+    }
   },
   actions: {
+    getAllProducts(context){
+      shop.getProducts(products => {
+        context.commit('setProducts', products);
+      })
+    }
   },
   modules: {
   }
